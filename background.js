5900f51f2af21aac8139e02c25161b3519228e22
JoshuaPHuang <joshuapinghuang@gmail.com>
2025-03-17 19:05:40 -0700

    Established .js connections
    - Added listener in background script to select Excel iframes and return information to popup.js
    - Began rewriting contentScript.js to read information from local memory/defaults.json and use this to suppress dialogs
    - Added "xpath" and "method" (REMOVE) to defaults.json so contentScript can parse it easily
    - Changed refreshCsMem to add itself to the memQueue
    - Successfully sending messages and responses between popup.js, contentScript.js, and background.js
    - Began filling in missing values in defaults.json

--- background.js
+++ background.js
@@ -24,3 +24,34 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 //         });
 //     });
 // });
+
+// Listen for the request to find all iframes with frame.url starting with "https://usc-excel.officeapps.live.com"
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "findIframes") {
+        // Get all open tabs
+        chrome.tabs.query({}, (tabs) => {
+            let allFrames = [];
+            let pendingPromises = tabs.map(tab => {
+                return new Promise((resolve) => {
+                    chrome.webNavigation.getAllFrames({ tabId: tab.id }, (frames) => {
+                        if (chrome.runtime.lastError || !frames) {
+                            return resolve([]);
+                        }
+                        // Filter iframes and add tab.id as frame.tabId to each frame
+                        // console.log("Frames:");
+                        // console.log(frames);
+                        let officeIframes = frames
+                        .filter(frame => frame.url.startsWith("https://usc-excel.officeapps.live.com"))
+                        .map(frame => ({ ...frame, customTabId: tab.id })); // Add tabId to each frame
+                        allFrames.push(...officeIframes);
+                        resolve();
+                    });
+                });
+            });
+            Promise.all(pendingPromises).then(() => {
+                sendResponse({ frames: allFrames });
+            });
+        });
+        return true; // Holds open for asynchronous response
+    }
+});
--- contentScript.js
+++ contentScript.js
@@ -21,6 +21,221 @@ function nearestXPath(element, xPath) {
     return null;
 }
 
+// Enable throttled notifications
+let last_notif_time = 0;
+function custom_notif(notif_txt)
+{
+    const now_time = Date.now();
+    if (now_time - last_notif_time >= 1000) {
+        chrome.runtime.sendMessage({type: 'custom_notif', text: notif_txt});
+        last_notif_time = now_time;
+    }
+};
+
+
+// ALLOW ACCESS TO MICROSOFT 365 ACCOUNT
+// document.evaluate("//span[contains(text(), 'Allow access to Microsoft 365 account')]", document, null, XPathelem.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+// <span id="8gdli5-Title" class="messageTitle-365">Allow access to Microsoft 365 account</span>
+// nearestXPath(elem, ".//button[@aria-label='Close']").click()
+
+// TRUST WORKBOOK LINKS?
+// searchXPath(document, "//*[contains(text(), 'Trust workbook links')]")
+// nearestXPath(elem, ".//button[@aria-label='Close']").click()
+
+// UNABLE TO REFRESH
+// searchXPath(document, "//*[contains(text(), 'UNABLE TO REFRESH')]")
+// nearestXPath(elem, ".//button[@aria-label='Close']").click()
+
+// STAY SIGNED IN (original)
+// "//span[contains(text(), 'Stay signed in')]"
+// click the closest button
+
+// STAY SIGNED IN (new? with more lenient button xpath)
+// "//*[contains(text(), 'Your session is about to expire')]"
+// nearestXPath(elem, ".//button[contains(@aria-label, 'Stay signed in') or contains(text(), 'Stay signed in')]")
+
+// Excel shortcuts enabled
+// "//label[contains(text(), 'Excel shortcuts enabled')]"
+// nearestXPath(elem, ".//button[@aria-label='Got it']").click()
+
+// EDITING SESSION IN PROGRESS
+// elem = searchXPath(document, "//div[contains(text(), 'Editing session in progress')]")
+// nearestXPath(elem, ".//button[@aria-label='Yes']").click()
+
+// SESSION EXPIRED (Sign In Again)
+// elem = searchXPath(document, "//span[contains(text(), 'Please sign in again to continue working')]")
+// nearestXPath(elem, ".//button[@aria-label='Sign in again']").click()
+
+
+
+// 1-3: NEED MORE WORKING SPACE?
+// elem = searchXPath(document, "//label[contains(text(), 'Need more working space')]")
+// nearestXPath(elem, ".//button[@aria-label='Close']").click()
+
+// 1-5: DISABLE CTRL MENU
+// elem = searchXPath(document, "//div[@id='PasteRecoveryUIGroup']")
+// nearestXPath(elem, ".//div[@id='ReactFloatie']") OR okay to just remove PasteRecoveryUIGroup
+
+// 2-1: SOMEONE HAS THIS WORKBOOK LOCKED
+// elem = searchXPath(document, "//*[contains(text(), 'Someone has this workbook locked')]")
+// nearestXPath(elem, ".//button[@aria-label='Continue in reading view']")
+
+// 2-2: FROZEN ROWS WON'T SCROLL
+// elem = searchXPath(document, "//div[contains(text(), 'Frozen rows won')]")
+// nearestXPath(elem, ".//button[@aria-label='OK']").click()
+
+
+
+
+
+
+
+
+
+// Define the observer globally so it can be managed
+let observer = null;
+
+
+
+// Run the functions to start the observer
+const iframePattern = new RegExp('https://.*\\.officeapps\\.live\\.com');
+// if (document.location.origin.match('https://usc-excel.officeapps.live.com'))
+if (document.location.origin.match(iframePattern))
+{
+    start_excel_observer();
+
+
+
+
+    // Add listener to re-initialize the excel_observer
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        if (request.action === "refreshMemory") {
+            console.log("Refreshing values...");
+            initXlObserver();
+            sendResponse({ status: "success" });  // Respond back if needed
+        }
+        return true;
+    });
+
+
+
+}
+
+
+
+
+
+
+
+function initXlObserver() {
+    // Add all rows to popup.html (after syncing defaults.json and local memory)
+    let defaultMemPromise = fetch(chrome.runtime.getURL("defaults.json")).then(response => response.json())// Get defaults dict from defaults.json file
+    let localMemPromise = new Promise((resolve) => { // Get the local memory from chrome local storage
+        chrome.storage.local.get(["xlpbMem"], (result) => {
+            resolve(result.xlpbMem || {});
+        });
+    });
+    // Wait until both local and default memories are ready
+    Promise.all([localMemPromise, defaultMemPromise]).then(([localMem, defaultMem]) => {
+        // Create list of local names with state=true
+        let trueNameArr = [];
+        for (let key in localMem) {
+            if (localMem[key].state === true) {
+                trueNameArr.push(localMem[key].name);
+            }
+        }
+        // Compare chrome local storage non-appended rows with defaults and update any rows which have differences (set state to false when updating)
+        let localModified = false;
+        let keySet = new Set([ // Get all keys to compare
+            ...Object.keys(localMem).filter(key => localMem[key].appended === false),
+            ...Object.keys(defaultMem),
+        ]);
+        keySet.forEach(key => {
+            let localItem = localMem[key];
+            let defaultItem = defaultMem[key];   
+            if (!localItem || JSON.stringify(localItem) !== JSON.stringify(defaultItem)) {
+                localMem[key] = defaultItem; // Overwrite
+                localModified = true;
+            }
+        });
+        // Set all local names that had state=true back to state=true
+        for (let key in localMem) {
+            if (trueNameArr.includes(localMem[key].name)) {
+                localMem[key].state = true;
+            }
+        }
+        // Update chrome local storage if needed
+        if (localModified) {
+            chrome.storage.local.set({ xlpbMem: localMem }, () => {
+                if (chrome.runtime.lastError) {
+                    console.error("Error saving to local storage:", chrome.runtime.lastError);
+                }
+            });
+        }
+        // Order keys numerically upwards
+        let sortedKeys = Object.keys(localMem).sort((a, b) => {
+            let numA = parseInt(a.split("-")[1], 10);
+            let numB = parseInt(b.split("-")[1], 10);
+            return numA - numB;
+        });
+        // Populate popup.html with all items from the current updated memory
+        for (let key of sortedKeys) {
+            // console.log(key)
+            // console.log(localMem[key])
+            appendRow(localMem[key]);
+        }
+        // Start the actual mutation observer
+        refreshMutObserver(localMem);
+    }).catch(error => {
+        console.log(error)
+        console.error("Error fetching data!")
+    });
+}
+
+
+function refreshMutObserver(memory) {
+    if (observer) {
+        observer.disconnect();
+        console.log('Disconnected previous observer.')
+    }
+    // Create new observer and assign it to the global var
+    observer = new MutationObserver(function(mutations) {
+
+    })
+}
+
+
+// Function to start the MutationObserver for the excel iframe
+function start_excel_observer() {
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.addedNodes.length > 0) { // If child elements were added
+
+                // Define an array of XPaths to check and remove elements
+                let rm_xpaths = [
+                    // "//div[@id='fluent-default-layer-host']",
+                    "//div[@class='RenamePromptCalloutHeader']",
+                    "//div[contains(@aria-label, 'Selected date')]",
+                    "//button[@id='KeyboardShortcutAwarenessCalloutCloseButton']/parent::*/parent::*/parent::*",
+                    "//div[contains(text(), 'Editing session in progress')]",
+                    "//div[contains(text(), 'Someone has this workbook locked')]",
+                    "//div[contains(text(), 'Sorry, your session has expired')]"
+                ];
+                // Call removeElementByXPath with the array of XPaths
+                removeElementByXPath(rm_xpaths);
+            }
+        });
+    });
+    observer.observe(document.body, {
+        childList: true, // Only listen to when child elements are added/removed
+        subtree: true // Listen to all descendants of document.body
+    });
+}
+
+
+
+
+
 // Function to check for the element using XPath and remove it if found
 function removeElementByXPath(xpathArray) {
     for (let xpath of xpathArray) {
@@ -71,121 +286,4 @@ function removeElementByXPath(xpathArray) {
     }
     return false; // Indicate that none of the XPaths matched any element
 }
-
-
-
-// ALLOW ACCESS TO MICROSOFT 365 ACCOUNT
-// document.evaluate("//span[contains(text(), 'Allow access to Microsoft 365 account')]", document, null, XPathelem.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-// <span id="8gdli5-Title" class="messageTitle-365">Allow access to Microsoft 365 account</span>
-// nearestXPath(elem, ".//button[@aria-label='Close']").click()
-
-// TRUST WORKBOOK LINKS?
-// searchXPath(document, "//*[contains(text(), 'Trust workbook links')]")
-// nearestXPath(elem, ".//button[@aria-label='Close']").click()
-
-// UNABLE TO REFRESH
-// searchXPath(document, "//*[contains(text(), 'UNABLE TO REFRESH')]")
-// nearestXPath(elem, ".//button[@aria-label='Close']").click()
-
-// STAY SIGNED IN (original)
-// "//span[contains(text(), 'Stay signed in')]"
-// click the closest button
-
-// STAY SIGNED IN (new? with more lenient button xpath)
-// "//*[contains(text(), 'Your session is about to expire')]"
-// nearestXPath(elem, ".//button[contains(@aria-label, 'Stay signed in') or contains(text(), 'Stay signed in')]")
-
-// Excel shortcuts enabled
-// "//label[contains(text(), 'Excel shortcuts enabled')]"
-// nearestXPath(elem, ".//button[@aria-label='Got it']").click()
-
-// EDITING SESSION IN PROGRESS
-// elem = searchXPath(document, "//div[contains(text(), 'Editing session in progress')]")
-// nearestXPath(elem, ".//button[@aria-label='Yes']").click()
-
-// FROZEN ROWS WON'T SCROLL
-// elem = searchXPath(document, "//div[contains(text(), 'Frozen rows won')]")
-// nearestXPath(elem, ".//button[@aria-label='OK']").click()
-
-// NEED MORE WORKING SPACE?
-// elem = searchXPath(document, "//label[contains(text(), 'Need more working space')]")
-// nearestXPath(elem, ".//button[@aria-label='Close']").click()
-
-// DISABLE CTRL MENU
-// elem = searchXPath(document, "//div[@id='PasteRecoveryUIGroup']")
-// nearestXPath(elem, ".//div[@id='ReactFloatie']") OR okay to just remove PasteRecoveryUIGroup
-
-// SOMEONE HAS THIS WORKBOOK LOCKED
-// elem = searchXPath(document, "//*[contains(text(), 'Someone has this workbook locked')]")
-// nearestXPath(elem, ".//button[@aria-label='Continue in reading view']")
-
-
-
-
-
-// Function to start the MutationObserver for the excel iframe
-function start_excel_observer() {
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (mutation.addedNodes.length > 0) { // If child elements were added
-
-                // Define an array of XPaths to check and remove elements
-                let rm_xpaths = [
-                    // "//div[@id='fluent-default-layer-host']",
-                    "//div[@class='RenamePromptCalloutHeader']",
-                    "//div[contains(@aria-label, 'Selected date')]",
-                    "//button[@id='KeyboardShortcutAwarenessCalloutCloseButton']/parent::*/parent::*/parent::*",
-                    "//div[contains(text(), 'Editing session in progress')]",
-                    "//div[contains(text(), 'Someone has this workbook locked')]",
-                    "//div[contains(text(), 'Sorry, your session has expired')]"
-                ];
-                // Call removeElementByXPath with the array of XPaths
-                removeElementByXPath(rm_xpaths);
-            }
-        });
-    });
-    observer.observe(document.body, {
-        childList: true, // Only listen to when child elements are added/removed
-        subtree: true // Listen to all descendants of document.body
-    });
-}
-
-// Enable throttled notifications
-let last_notif_time = 0;
-function custom_notif(notif_txt)
-{
-    const now_time = Date.now();
-    if (now_time - last_notif_time >= 1000) {
-        chrome.runtime.sendMessage({type: 'custom_notif', text: notif_txt});
-        last_notif_time = now_time;
-    }
-};
-
-// Run the functions to start the observer
-const iframePattern = new RegExp('https://.*\\.officeapps\\.live\\.com');
-// if (document.location.origin.match('https://usc-excel.officeapps.live.com'))
-if (document.location.origin.match(iframePattern))
-{
-    start_excel_observer();
-
-
-    ///////////////////////////////////////////////////////////////////////
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        if (message && message.message === "Hello from popup!") {
-            // Log the message received
-            console.log("Received message in iframe:", message.message);
-            sendResponse({status: "Message received and processed."});
-        }
-    });
-
-
-
-
-}
-
-
-
-
-
-
 
